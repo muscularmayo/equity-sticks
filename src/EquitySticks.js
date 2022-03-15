45f0c30a42bb8e7bdde08c3cc372b9a92131d67d
Chris Risden <christopherrisden@gmail.com>
2022-03-14 17:19:48 -0700

    off to an interesting start - gotta figure out my components

---
+++ src/EquitySticks.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EquitySticks extends Component {
+  render () {
+    return (
+    <div>
+      Equity Sticks
+    </div>
+    )
+  };
+}
+
+export default EquitySticks;
